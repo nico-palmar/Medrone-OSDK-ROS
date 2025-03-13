b5a12e447261098a3c2b8c441fb6814d7767b249
nico-palmar <nicolas.palmar8@gmail.com>
2025-03-13 15:40:05 -0400

    increaase mission duration

--- src/dji_osdk_ros/samples/msdk_osdk_node.cpp
+++ src/dji_osdk_ros/samples/msdk_osdk_node.cpp
@@ -221,7 +221,7 @@ private:
         ac_.sendGoal(goal);
 
         // Wait for the result
-        const auto finished_before_timeout = ac_.waitForResult(ros::Duration(20.0));
+        const auto finished_before_timeout = ac_.waitForResult(ros::Duration(200.0));
         
         if (!finished_before_timeout)
         {
