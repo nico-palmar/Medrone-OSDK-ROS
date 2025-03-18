ddefa7c90aa09d6a73a8324f3fbd82aa6f046abd
nico-palmar <nicolas.palmar8@gmail.com>
2025-03-18 13:08:36 -0400

    tune gains a bit more

--- src/dji_osdk_ros/samples/move_to_waypoint.cpp
+++ src/dji_osdk_ros/samples/move_to_waypoint.cpp
@@ -224,7 +224,7 @@ private:
     osdk::MoveToWaypointFeedback feedback_;
 
     // gain for each axis... x, y, and z
-    const Eigen::Vector3d Kp { 0.6, 0.6, 0.4 };
+    const Eigen::Vector3d Kp { 0.8, 0.8, 0.6 };
 
     // velocity saturation [m/s]
     // max magnitude of the velocity vector
