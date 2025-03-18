91cd44f7f19df5ce32e2d7f6ef89be82b0faeed4
nico-palmar <nicolas.palmar8@gmail.com>
2025-03-18 13:16:43 -0400

    lower saturation and tune gains more

--- src/dji_osdk_ros/samples/move_to_waypoint.cpp
+++ src/dji_osdk_ros/samples/move_to_waypoint.cpp
@@ -224,11 +224,11 @@ private:
     osdk::MoveToWaypointFeedback feedback_;
 
     // gain for each axis... x, y, and z
-    const Eigen::Vector3d Kp { 0.8, 0.8, 0.6 };
+    const Eigen::Vector3d Kp { 0.7, 0.7, 0.6 };
 
     // velocity saturation [m/s]
     // max magnitude of the velocity vector
-    const float speed_saturation_ { 20.0 };
+    const float speed_saturation_ { 15.0 };
 
     Eigen::Vector3d measured_speed_;
 
